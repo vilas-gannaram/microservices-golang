0223102121d74d73559896f480163d71f7ca5b40
vilas-gannaram <vilasgannaram@gmail.com>
2026-05-23 22:31:37 +0530

    add: implement broker service with Docker support

---
+++ broker-service/cmd/api/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const webPort = "8000"
+
+type Config struct{}
+
+func main() {
+	app := Config{}
+
+	log.Printf("Starting broker service on port %s", webPort)
+
+	// define http server
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%s", webPort),
+		Handler: app.routes(),
+	}
+
+	// start the server
+	if err := srv.ListenAndServe(); err != nil {
+		log.Panic(err)
+	}
+}
